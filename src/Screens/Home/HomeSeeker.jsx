0d305d1c9e98903e70556199ba12fd9e0141bc53
aayutha <thatheraaayushi@gmail.com>
2024-05-26 21:40:51 +0530

    added filter functionality

--- src/Screens/Home/HomeSeeker.jsx
+++ src/Screens/Home/HomeSeeker.jsx
@@ -1,4 +1,13 @@
-import {ActivityIndicator, FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {useContext, useEffect, useRef, useState} from 'react';
 import Header from '../../Components/Header';
 import {Color} from '../../Constants/Color';
@@ -6,19 +15,19 @@ import {windowHeight} from '../../Constants/Dimension';
 import JobCategoryBox from '../../Components/SeekerComp/JobCategoryBox';
 import SearchBar from '../../Components/SeekerComp/SearchBar';
 import Typoghraphy from '../../Components/Typoghraphy';
-import { useIsFocused } from '@react-navigation/native';
-import { GlobalVariable } from '../../../App';
-import { getAllJobs } from '../../Network/Seeker/Home';
+import {useIsFocused} from '@react-navigation/native';
+import {GlobalVariable} from '../../../App';
+import {getAllJobs} from '../../Network/Seeker/Home';
 import JobFilter from './JobFilter';
 
 const HomeSeeker = ({navigation}) => {
-  const jobFilterRef=useRef();
-  const [loading,setLoading]=useState(false);
+  const jobFilterRef = useRef();
+  const [loading, setLoading] = useState(false);
   const [Jobs, setJobs] = useState([]);
   const [SearchPro, setSearchPro] = useState([]);
-  const [searchValue,setSearchValue]=useState("");
-  const isFocused=useIsFocused();
-  const {userDetails} = useContext(GlobalVariable);  
+  const [searchValue, setSearchValue] = useState('');
+  const isFocused = useIsFocused();
+  const {userDetails} = useContext(GlobalVariable);
 
   useEffect(() => {
     getAllJobsLocal();
@@ -26,24 +35,23 @@ const HomeSeeker = ({navigation}) => {
 
   const getAllJobsLocal = async () => {
     try {
-      let jobs=await getAllJobs(userDetails);
-      if(jobs?.error)
-        throw jobs?.data;
+      let jobs = await getAllJobs(userDetails);
+      if (jobs?.error) throw jobs?.data;
       setJobs(jobs?.data);
       setSearchPro(jobs?.data);
     } catch (error) {
-      console.log(error)
-    }finally{
+      console.log(error);
+    } finally {
       setLoading(false);
     }
   };
 
-  const FilterFunc = (searchItem) => {
+  const FilterFunc = searchItem => {
     setSearchValue(searchItem);
-    if(!searchItem){
-      setSearchPro(Jobs)
-    }else{
-      console.log(searchItem,"kn");
+    if (!searchItem) {
+      setSearchPro(Jobs);
+    } else {
+      console.log(searchItem, 'kn');
       const searcheShops = Jobs.filter(filteredShops => {
         return Object.values(filteredShops)
           .join(' ')
@@ -58,10 +66,24 @@ const HomeSeeker = ({navigation}) => {
       }
     }
   };
-
+  
+  const applyFilters = (prefferedRoles, sliderValue) => {
+    if (prefferedRoles.length === 0 && sliderValue === 0) {
+      setSearchPro(Jobs);
+    } else {
+      const filterJobs = Jobs.filter(job => {
+        const matchRoles = job.jobRole.some(role =>
+          prefferedRoles.includes(role),
+        );
+        const matchSalary = job.Salart >= sliderValue;
+        return matchRoles && matchSalary;
+      });
+      setSearchPro(filterJobs);
+    }
+  };
   return (
     <View style={{height: windowHeight, backgroundColor: Color.White}}>
-      <JobFilter ref={jobFilterRef} />
+      <JobFilter ref={jobFilterRef} onApplyFilter={applyFilters} />
       <Header title={'Find Jobs'} />
       <View style={styles.ContentContainer}>
         <SearchBar
@@ -69,35 +91,46 @@ const HomeSeeker = ({navigation}) => {
           searchedValue={searchValue}
           elevation={0}
           borderWidth={1}
-          borderColor={Color.Light_grey} 
+          borderColor={Color.Light_grey}
           backgroundColor={Color.Light_grey}
-          onSearchValue={value=>FilterFunc(value)}
+          onSearchValue={value => FilterFunc(value)}
         />
-        <View style={{ width: '90%', alignSelf: 'center' }}>
+        <View style={{width: '90%', alignSelf: 'center'}}>
           <TouchableOpacity
             onPress={() => jobFilterRef.current.openOptions()}
-            style={[{alignItems: 'center',paddingHorizontal:10,height: 30,alignSelf: 'flex-end',borderRadius: 4,justifyContent: 'space-evenly',backgroundColor: Color.Light_grey,fontWeight:'600'},]}
-          >
-            <Text
-              style={[styles.textStyle,{  },]}
-            >
-              Apply Filter
-            </Text>
+            style={[
+              {
+                alignItems: 'center',
+                paddingHorizontal: 10,
+                height: 30,
+                alignSelf: 'flex-end',
+                borderRadius: 4,
+                justifyContent: 'space-evenly',
+                backgroundColor: Color.Light_grey,
+                fontWeight: '600',
+              },
+            ]}>
+            <Text style={[styles.textStyle, {}]}>Apply Filter</Text>
           </TouchableOpacity>
         </View>
-        {
-          loading ? <ActivityIndicator size={30} color={Color.LightBlue} />:
+        {loading ? (
+          <ActivityIndicator size={30} color={Color.LightBlue} />
+        ) : (
           <FlatList
             ListEmptyComponent={
-              <Typoghraphy size={16} color={Color.Grey} fontWeight="700" style={{textAlign:"center",marginTop:20}}>
+              <Typoghraphy
+                size={16}
+                color={Color.Grey}
+                fontWeight="700"
+                style={{textAlign: 'center', marginTop: 20}}>
                 No Job Found
               </Typoghraphy>
             }
-            data={SearchPro} 
+            data={SearchPro}
             keyExtractor={(item, index) => index.toString()}
             showsVerticalScrollIndicator={false}
-            style={{ height: windowHeight / 1.32 }}
-            renderItem={({ item, index }) => (
+            style={{height: windowHeight / 1.32}}
+            renderItem={({item, index}) => (
               <JobCategoryBox
                 key={index}
                 Sector={item?.CompanyName}
@@ -106,15 +139,15 @@ const HomeSeeker = ({navigation}) => {
                 Salary={item?.Salart}
                 MinExp={item?.MinExp ? `${item?.MinExp} years` : undefined}
                 Mode={item?.JobMode}
-                applicants={item?.interestedUsers?item?.interestedUsers:[]}
+                applicants={item?.interestedUsers ? item?.interestedUsers : []}
                 Desc={item?.JobDesc.substr(0, 200)}
                 onPress={() => {
-                  navigation.navigate('JobDescription', { JobDetail: item });
+                  navigation.navigate('JobDescription', {JobDetail: item});
                 }}
               />
             )}
           />
-        }
+        )}
       </View>
     </View>
   );
--- src/Screens/Home/JobFilter.jsx
+++ src/Screens/Home/JobFilter.jsx
@@ -1,162 +1,182 @@
-import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, Modal, Text, ActivityIndicator } from 'react-native';
-import { windowWidth } from '../../Constants/Dimension';
-import { Color } from '../../Constants/Color';
-import { getJobRoles } from '../../Network/HelperFunctions/common';
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
+import {windowWidth} from '../../Constants/Dimension';
+import {Color} from '../../Constants/Color';
+import {getJobRoles} from '../../Network/HelperFunctions/common';
 import Typoghraphy from '../../Components/Typoghraphy';
 import {Slider} from '@miblanchard/react-native-slider';
 import Button from '../../Components/Button';
 
+const JobFilter = forwardRef(({ onApplyFilter }, ref) => {
+  const [openPopup, setOpenPopup] = useState(false);
+  const [prefferedRoles, setPrefferedROles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [jobRoles, setJobRoles] = useState([]);
+  const [sliderValue, setSliderValues] = useState(0);
 
-const JobFilter = forwardRef(({ onPress }, ref) => {
-    const [openPopup, setOpenPopup] = useState(false);
-    const [prefferedRoles,setPrefferedROles]=useState([]);
-    const [loading,setLoading]=useState(false);
-    const [jobRoles,setJobRoles]=useState([]);
-    const [sliderValue,setSliderValues]=useState(0);
+  useEffect(() => {
+    getRoles();
+  }, []);
 
-    useEffect(()=>{
-        getRoles();
-    },[]);
-
-    const getRoles=async()=>{
-        try {
-            setLoading(true);
-            const data=await getJobRoles();
-            if(data?.error)
-                throw data?.data;
-            let roles=data?.data?.find((item)=>item?.id === "jobRoles");
-            setJobRoles(roles?.role);
-        } catch (error) {
-            console.log(error,"errior rtole");
-        } finally{
-            setLoading(false);
-        }
+  const getRoles = async () => {
+    try {
+      setLoading(true);
+      const data = await getJobRoles();
+      if (data?.error) throw data?.data;
+      let roles = data?.data?.find(item => item?.id === 'jobRoles');
+      setJobRoles(roles?.role);
+    } catch (error) {
+      console.log(error, 'errior rtole');
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useImperativeHandle(ref, () => ({
-        openOptions() {
-            setOpenPopup(true);
-        },
-        closeOptions() {
-            setOpenPopup(false);
-        },
-    }));
+  useImperativeHandle(ref, () => ({
+    openOptions() {
+      setOpenPopup(true);
+    },
+    closeOptions() {
+      setOpenPopup(false);
+    },
+    getFilters() {
+      return {
+        prefferedRoles,
+        sliderValue,
+      };
+    },
+  }));
+  
+  const filterData = () => {
+    onApplyFilter(prefferedRoles, sliderValue);
+   
+    setOpenPopup(false);
+  };
 
-    const filterData=()=>{
-        console.log(prefferedRoles);
-        console.log(sliderValue);
-        setOpenPopup(false);
-    }
-
-    return (
-        <Modal
-        visible={openPopup}
-        animationType="slide"
-        transparent={true}
-        onRequestClose={() => setOpenPopup(false)}
-        >
-            <View style={styles.modeOuter}>
-                <View style={styles.innnerModel}>
-                    <Text style={{color:Color.Black,fontWeight:'600',}} >
-                        Job Role
-                    </Text>
-                    <View
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                            flexWrap: 'wrap',
-                        }}
-                    >
-                    {
-                        loading ? <ActivityIndicator size={20} color={Color.ThemeBlue} style={{marginTop:10,alignSelf:'center'}} />:
-                        jobRoles?.length >0 && 
-                        jobRoles?.map((item)=>(
-                        <TouchableOpacity
-                            onPress={() => {
-                                if(prefferedRoles.includes(item)){
-                                let newRoles=[];
-                                    newRoles=prefferedRoles.filter((value)=> value != item);
-                                    setPrefferedROles(newRoles);
-                                }else{
-                                    setPrefferedROles([...prefferedRoles,item]);
-                                }
-                            }}
-                            style={{
-                                borderWidth: 0.5,
-                                marginRight: 5,
-                                borderRadius: 8,
-                                padding: 5,
-                                marginVertical: 5,
-                                backgroundColor: prefferedRoles.includes(item)?Color.ThemeBlue:Color.White,
-                                borderWidth: 0.5,
-                            }}>
-                            <Typoghraphy
-                                size={11}
-                                color={prefferedRoles.includes(item) ? Color.White : Color.Black}
-                            >
-                                {item}
-                            </Typoghraphy>
-                            </TouchableOpacity>
-                        ))
+  return (
+    <Modal
+      visible={openPopup}
+      animationType="slide"
+      transparent={true}
+      onRequestClose={() => setOpenPopup(false)}>
+      <View style={styles.modeOuter}>
+        <View style={styles.innnerModel}>
+          <Text style={{color: Color.Black, fontWeight: '600'}}>Job Role</Text>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+            }}>
+            {loading ? (
+              <ActivityIndicator
+                size={20}
+                color={Color.ThemeBlue}
+                style={{marginTop: 10, alignSelf: 'center'}}
+              />
+            ) : (
+              jobRoles?.length > 0 &&
+              jobRoles?.map(item => (
+                <TouchableOpacity
+                  onPress={() => {
+                    if (prefferedRoles.includes(item)) {
+                      let newRoles = [];
+                      newRoles = prefferedRoles.filter(value => value != item);
+                      setPrefferedROles(newRoles);
+                    } else {
+                      setPrefferedROles([...prefferedRoles, item]);
                     }
-                    </View>
-                    <Text style={{color:Color.Black,fontWeight:'600',marginTop:10}} >
-                        Expected Salary
-                    </Text>
-                    <Slider
-                        // value={sliderValue}
-                        onValueChange={value => setSliderValues(Math.round(value*100))}
-                    />
-                    <Button
-                        onPress={filterData}
-                        BtnStyle={[
-                            styles.BtnStyle,
-                            {borderWidth: 2, borderColor: Color.ThemeBlue},
-                        ]}
-                        BtnTxtStyle={[styles.BtnTxtStyle]}
-                        title={'Search Job'}
-                    />
-                    {/* <Text style={{color:Color.Black,fontWeight:'600',marginTop:10}}>Value: {sliderValue}</Text> */}
-                </View>
-            </View>
-        </Modal>
-    );
+                  }}
+                  style={{
+                    borderWidth: 0.5,
+                    marginRight: 5,
+                    borderRadius: 8,
+                    padding: 5,
+                    marginVertical: 5,
+                    backgroundColor: prefferedRoles.includes(item)
+                      ? Color.ThemeBlue
+                      : Color.White,
+                    borderWidth: 0.5,
+                  }}>
+                  <Typoghraphy
+                    size={11}
+                    color={
+                      prefferedRoles.includes(item) ? Color.White : Color.Black
+                    }>
+                    {item}
+                  </Typoghraphy>
+                </TouchableOpacity>
+              ))
+            )}
+          </View>
+          <Text style={{color: Color.Black, fontWeight: '600', marginTop: 10}}>
+            Expected Salary
+          </Text>
+          <Slider
+            // value={sliderValue}
+            onValueChange={value => setSliderValues(Math.round(value * 1000))}
+          />
+          <Button
+            onPress={filterData}
+            BtnStyle={[
+              styles.BtnStyle,
+              {borderWidth: 2, borderColor: Color.ThemeBlue},
+            ]}
+            BtnTxtStyle={[styles.BtnTxtStyle]}
+            title={'Search Job'}
+          />
+          {/* <Text style={{color:Color.Black,fontWeight:'600',marginTop:10}}>Value: {sliderValue}</Text> */}
+        </View>
+      </View>
+    </Modal>
+  );
 });
 
 const styles = StyleSheet.create({
-    modeOuter: {
-        backgroundColor: '#000000aa',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      innnerModel: {
-        backgroundColor: 'white',
-        borderRadius: 10,
-        width: windowWidth - 40,
-        paddingBottom: 20,
-        elevation: 10,
-        shadowColor: 'white',
-        // alignItems: 'center',
-        paddingHorizontal: 14,
-        paddingTop: 20,
-    },
-    BtnStyle: {
-        paddingVertical: 5,
-        paddingHorizontal: 20,
-        borderRadius: 6,
-        width: '50%',
-        marginTop: 2,
-        alignSelf: 'center',
-        backgroundColor: Color.ThemeBlue,
-        height:40
-      },
-      BtnTxtStyle: {
-        fontSize: 16,
-        fontWeight: '700',
-        color: Color.White,
-    },
+  modeOuter: {
+    backgroundColor: '#000000aa',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innnerModel: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: windowWidth - 40,
+    paddingBottom: 20,
+    elevation: 10,
+    shadowColor: 'white',
+    // alignItems: 'center',
+    paddingHorizontal: 14,
+    paddingTop: 20,
+  },
+  BtnStyle: {
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    borderRadius: 6,
+    width: '50%',
+    marginTop: 2,
+    alignSelf: 'center',
+    backgroundColor: Color.ThemeBlue,
+    height: 40,
+  },
+  BtnTxtStyle: {
+    fontSize: 16,
+    fontWeight: '700',
+    color: Color.White,
+  },
 });
 export default JobFilter;
